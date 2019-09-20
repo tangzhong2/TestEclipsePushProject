b8e5070cdf112abce633bfe008ae4232e9149e65
tangzhong2 <49061990+tangzhong2@users.noreply.github.com>
2019-09-20 10:05:44 +0800

    change file

--- src/com/tangzhong/HelloWord.java
+++ src/com/tangzhong/HelloWord.java
@@ -3,5 +3,6 @@ package com.tangzhong;
 public class HelloWord {
 	public static void main(String[] args) {
 		System.out.println("Test push project to github");
+		System.out.println("github content change");
 	}
 }
