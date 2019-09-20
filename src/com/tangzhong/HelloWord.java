136eb96fd00f3f769ea821e57357919e75a048ac
tangzhong <2917411506@qq.com>
2019-09-20 10:09:01 +0800

    change02

--- src/com/tangzhong/HelloWord.java
+++ src/com/tangzhong/HelloWord.java
@@ -4,5 +4,6 @@ public class HelloWord {
 	public static void main(String[] args) {
 		System.out.println("Test push project to github");
 		System.out.println("github content change");
+		System.out.println("Test push project to github change02");
 	}
 }
