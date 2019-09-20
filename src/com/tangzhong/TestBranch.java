afa7fff2d214b94011f9d8f378e0cead4ea2dd63
tangzhong <2917411506@qq.com>
2019-09-20 10:18:44 +0800

    测试Eclipse 合并分支

--- src/com/tangzhong/TestBranch.java
+++ src/com/tangzhong/TestBranch.java
@@ -4,5 +4,6 @@ public class TestBranch {
 	public static void main(String[] args) {
 		System.out.println("Test Branch");
 		System.out.println("Test Branch02");
+		System.out.println("测试Eclipse 合并分支");
 	}
 }
